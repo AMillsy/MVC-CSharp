cb5780c1a6121402839251c3f2cf2fcf9cd3da41
AMillsy <00adammills00@gmail.com>
2023-09-26 12:25:36 +0100

    Using bootswatch/bootstrap for styling and setting up a create category page

--- BulkyBookWeb/Controllers/CategoryController.cs
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -19,5 +19,10 @@ namespace BulkyBookWeb.Controllers
             IEnumerable<Category> objCategoryList = _db.Categories;
             return View(objCategoryList);
         }
+        //Get
+        public IActionResult Create()
+        {
+            return View();
+        }
     }
 }
